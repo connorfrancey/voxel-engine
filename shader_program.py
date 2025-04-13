a4ed93ad8fb11692600ba30bd8dcdd928290efa6
connorfrancey <connorfranceydev@gmail.com>
2025-04-13 01:01:03 -0600

    Texture array

--- shader_program.py
+++ shader_program.py
@@ -21,7 +21,7 @@ class ShaderProgram:
         # Chunk
         self.chunk['m_proj'].write(self.player.m_proj)
         self.chunk['m_model'].write(glm.mat4())
-        self.chunk['u_texture_0'] = 0
+        self.chunk['u_texture_array_0'] = 1
 
         # Voxel marker
         self.voxel_marker['m_proj'].write(self.player.m_proj)
